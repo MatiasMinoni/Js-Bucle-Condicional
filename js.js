ba79e3c5d738c23a2132292c00349ffc062f2172
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2022-04-19 03:11:05 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,64 @@
+
+
+//  let historia = prompt("Quieres que  inicie la historia?. si-no");
+
+// if (historia == "si"){
+//     document.write("Empieza la historia")
+// }
+// else if(historia == "no"){
+//     document.write("No empieza la historia")
+// }
+// else if(historia != ""){
+//     document.write(" la historia")
+// }
+
+
+// var historia = prompt("Elije una historia para comenzar. 1-2-3");
+
+// switch ((historia)) {
+//     case '1':
+//         document.write("El cuento de la lechera <br> Érase una vez una joven lechera que llevaba un cubo de leche en la cabeza, camino al mercado para venderla. <br >Durante el camino, la soñadora joven iba imaginando lo que podría lograr conseguir con la leche. Pensó que en primer lugar y con el dinero de la venta compraría un canasto de huevos, los cuales una vez eclosionaran le permitiría montar una pequeña granja de pollos.<br> Una vez estos crecieran podría venderlos, lo que le daría dinero para comprarse un lechón. Una vez este creciera la venta del animal bastaría para comprarse una ternera, con la leche de la cual seguiría obteniendo beneficios y a su vez podría tener terneros. Sin embargo, mientras iba pensando todas estas cosas la joven tropezó, lo que provocó que el cántaro cayera el suelo y se rompiera. Y con él, sus expectativas hacia lo que podría haber hecho con ella.”  Este cuento, que cuenta con versiones de Esopo y La Fontaine (siendo este último el que hemos reflejado), nos enseña la necesidad de vivir en el presente y que a pesar de que soñar es necesario también debemos tener en cuenta que ello no basta para lograr nuestros propósitos. Inicialmente, es una pequeña historia que nos avisa de tener cuidado con que la ambición no nos haga perder el sentido. Asimismo, en algunas adaptaciones se incluye también un diálogo posterior entre la lechera y su madre, quien le cuenta que gracias a tener fantasías parecidas pudo lograr montar una granja: en este caso es una reflexión de que necesitamos soñar y ambicionar, pero cuidando lo que hacemos para llegar a cumplir los objetivos, además de no rendirnos ante el primer tropiezo u obstáculo.");
+//   break
+//     case '2':
+//         document.write("La sospecha <br> Érase una vez un leñador el cual un día se dio cuenta que no tenía su hacha. Sorprendido y con lágrimas en los ojos, se encontró cerca de su casa al vecino, quien como siempre lo hacía le saludó sonriente y amablemente.  Mientras éste entraba en su casa, el leñador de repente empezó a sospechar y pensar que tal vez hubiese sido el vecino quien le había robado el hacha. De hecho, ahora que lo pensaba bien su sonrisa parecía nerviosa, tenía una mirada extraña e incluso hubiese dicho que le temblaban las manos. Bien pensado, el vecino tenía la misma expresión que un ladrón, caminaba como un ladrón y hablaba como un ladrón.    Todo ello iba pensando el leñador, cada vez más convencido de haber encontrado al culpable del hurto, cuando de repente se dió cuenta de que sus pasos le habían llevado de nuevo al bosque donde había estado la noche anterior.   De pronto, tropezó con algo duro y cayó. Cuando miró al suelo...encontró su hacha! El leñador volvió de nuevo a su hogar con el hacha, arrepentido de sus sospechas, y cuando vio de nuevo a su vecino vio que su expresión, andar y manera de hablar eran (y habían sido en todo momento) las de siempre.”  Esta historia corta, la cual forma parte de muchas tradiciones pero al parecer tiene su origen en China, nos sirve para aprender que a veces nuestros pensamientos y sospechas nos hacen tener percepciones distorsionadas de la realidad, pudiendo llegar a malinterpretar situaciones y personas con gran facilidad. También nos enseña a no acusar a alguien gratuitamente hasta tener pruebas reales de aquello de lo que le acusamos.");
+//     break
+//     case '3':
+//         document.write("La gallina de los huevos de oro  <br> Érase una vez una pareja de granjeros que, un día, descubrieron en uno de los nidos en los que criaban gallinas un huevo de oro macizo. La pareja fue observando que el ave producía tal prodigio día tras día, obteniendo cada día un huevo de oro    Reflexionando sobre qué era lo que hacía que la gallina en cuestión tuviese esa habilidad, sospecharon que que ésta poseía oro en su interior. Para comprobarlo y obtener todo el oro de una vez, mataron a la gallina y la abrieron, descubriendo para su sorpresa que por dentro la prodigiosa ave era igual a las demás. Y también se dieron cuenta que, en su ambición, habían acabado con aquello que les había estado enriqueciendo.”    Esta fábula, asociada a Esopo aunque también versionada por autores como Samariaga o La Fontaine y que en ocasiones nos habla de una gallina y en otras de un ganso, nos enseña la importancia de dejar de lado la codicia, ya que nos puede conducir a perder lo que tenemos.");
+//   break
+//     default: 
+//         document.write("No seleccionaste ninguna historia");
+        
+
+//   }
+ 
+// let historia = prompt("Seleccione una historia");
+
+
+
+//  if (historia == "1")
+// document.write("Comenzará la historia 1 ");
+// else if (historia == "2")
+// document.write("Comenzará la historia 2 ");
+// else if (historia == "3")
+// document.write("Comenzará la historia 3 ");
+// else (historia == "4")
+// document.write("Comenzará la historia 4 ");
+
+
+
+
+var historia
+do {
+    historia = prompt("Seleccione una historia. 1-2-3");
+    if (historia == "1")
+    document.write("Comenzará la historia 1 <br> El cuento de la lechera: <br> Érase una vez una joven lechera que llevaba un cubo de leche en la cabeza, camino al mercado para venderla. <br >Durante el camino, la soñadora joven iba imaginando lo que podría lograr conseguir con la leche. Pensó que en primer lugar y con el dinero de la venta compraría un canasto de huevos, los cuales una vez eclosionaran le permitiría montar una pequeña granja de pollos.<br> Una vez estos crecieran podría venderlos, lo que le daría dinero para comprarse un lechón. Una vez este creciera la venta del animal bastaría para comprarse una ternera, con la leche de la cual seguiría obteniendo beneficios y a su vez podría tener terneros. Sin embargo, mientras iba pensando todas estas cosas la joven tropezó, lo que provocó que el cántaro cayera el suelo y se rompiera. Y con él, sus expectativas hacia lo que podría haber hecho con ella.”  Este cuento, que cuenta con versiones de Esopo y La Fontaine (siendo este último el que hemos reflejado), nos enseña la necesidad de vivir en el presente y que a pesar de que soñar es necesario también debemos tener en cuenta que ello no basta para lograr nuestros propósitos. Inicialmente, es una pequeña historia que nos avisa de tener cuidado con que la ambición no nos haga perder el sentido. Asimismo, en algunas adaptaciones se incluye también un diálogo posterior entre la lechera y su madre, quien le cuenta que gracias a tener fantasías parecidas pudo lograr montar una granja: en este caso es una reflexión de que necesitamos soñar y ambicionar, pero cuidando lo que hacemos para llegar a cumplir los objetivos, además de no rendirnos ante el primer tropiezo u obstáculo.");
+
+    else if (historia == "2")
+    document.write("Comenzará la historia 2 <br> La sospecha: <br> Érase una vez un leñador el cual un día se dio cuenta que no tenía su hacha. Sorprendido y con lágrimas en los ojos, se encontró cerca de su casa al vecino, quien como siempre lo hacía le saludó sonriente y amablemente.  Mientras éste entraba en su casa, el leñador de repente empezó a sospechar y pensar que tal vez hubiese sido el vecino quien le había robado el hacha. De hecho, ahora que lo pensaba bien su sonrisa parecía nerviosa, tenía una mirada extraña e incluso hubiese dicho que le temblaban las manos. Bien pensado, el vecino tenía la misma expresión que un ladrón, caminaba como un ladrón y hablaba como un ladrón.    Todo ello iba pensando el leñador, cada vez más convencido de haber encontrado al culpable del hurto, cuando de repente se dió cuenta de que sus pasos le habían llevado de nuevo al bosque donde había estado la noche anterior.   De pronto, tropezó con algo duro y cayó. Cuando miró al suelo...encontró su hacha! El leñador volvió de nuevo a su hogar con el hacha, arrepentido de sus sospechas, y cuando vio de nuevo a su vecino vio que su expresión, andar y manera de hablar eran (y habían sido en todo momento) las de siempre.”  Esta historia corta, la cual forma parte de muchas tradiciones pero al parecer tiene su origen en China, nos sirve para aprender que a veces nuestros pensamientos y sospechas nos hacen tener percepciones distorsionadas de la realidad, pudiendo llegar a malinterpretar situaciones y personas con gran facilidad. También nos enseña a no acusar a alguien gratuitamente hasta tener pruebas reales de aquello de lo que le acusamos. ");
+    else if (historia == "3")
+    document.write("Comenzará la historia 3  <br> La gallina de los huevos de oro:  <br> Érase una vez una pareja de granjeros que, un día, descubrieron en uno de los nidos en los que criaban gallinas un huevo de oro macizo. La pareja fue observando que el ave producía tal prodigio día tras día, obteniendo cada día un huevo de oro    Reflexionando sobre qué era lo que hacía que la gallina en cuestión tuviese esa habilidad, sospecharon que que ésta poseía oro en su interior. Para comprobarlo y obtener todo el oro de una vez, mataron a la gallina y la abrieron, descubriendo para su sorpresa que por dentro la prodigiosa ave era igual a las demás. Y también se dieron cuenta que, en su ambición, habían acabado con aquello que les había estado enriqueciendo.”    Esta fábula, asociada a Esopo aunque también versionada por autores como Samariaga o La Fontaine y que en ocasiones nos habla de una gallina y en otras de un ganso, nos enseña la importancia de dejar de lado la codicia, ya que nos puede conducir a perder lo que tenemos.");
+
+
+} while (historia >= "4" || historia <= "0");
+
+
